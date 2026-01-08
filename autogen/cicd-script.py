c39d6a653ca980af0bac516282270df8858e1ac2
me-man-dave <189643182+davebargainspacescostly@users.noreply.github.com>
2026-01-08 16:27:40 +0000

    update auto gen script

--- autogen/cicd-script.py
+++ autogen/cicd-script.py
@@ -23,13 +23,33 @@ This is where you will find out about a {{ animal }}
 [Deploy a {{ animal }}](https://giphy.com/{{ animal }})
 """
 
+mkdocs_template = """site_name: 'Example with Animals'
+site_description: An informative description
+plugins:
+  - techdocs-core
+nav:
+  - Overview: \'index.md\'
+  - What is a fish: \'fish.md\'
+  - All the Animals: \'animals/all.md\'
+  - Each Animal:
+{%- for animal in animals %}
+    - {{ animal }}: \'animals/{{ animal }}.md\'
+{%- endfor %}
+"""
+
 j2_template = Template(animal_page_template)
 for animal in animals:
   print(f'saving to filename docs/animals/{animal}.md')
-  with open('docs/animals/'+animal+'.md', "w") as text_file:    
+  with open('docs/animals/'+animal+'.md', "w") as text_file:
     text_file.write(j2_template.render(animal=animal))
 
 j2_template = Template(all_animals_page_template)
 print('saving to filename docs/animals/all.md')
 with open('docs/animals/all.md', "w") as text_file:
   text_file.write(j2_template.render(animals=animals))
+
+j2_template = Template(mkdocs_template)
+print('saving to filename mkdocs.yml')
+with open('mkdocs.yml', "w") as text_file:
+  #text_file.write(j2_template.render(animals=animals))
+  print(j2_template.render(animals=animals))
